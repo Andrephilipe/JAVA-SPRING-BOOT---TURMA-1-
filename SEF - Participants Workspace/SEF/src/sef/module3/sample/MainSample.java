e1295ac2ffb8686b4c07a3c3f7165be0a67a3b35
André Cassiano <andre.p.cassiano@accenture.com>
2022-01-31 19:47:36 -0300

    Subindo os arquivos do curso

---
+++ SEF - Participants Workspace/SEF/src/sef/module3/sample/MainSample.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Jan 31, 2022
+ * 
+ * Hello World Program
+ */
+
+package sef.module3.sample;
+import java.lang.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+* @author André Philipe
+*/
+public class MainSample {
+
+	public static void main(String[] args) {
+		//	This line prints out the String 'Hello World!' in the console
+		System.out.println( "Welcome to Java!");
+		
+	}
+}
