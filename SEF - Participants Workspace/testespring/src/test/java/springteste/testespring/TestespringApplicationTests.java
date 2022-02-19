17038389fc6ef3e7af739f72d73672969fccbbdd
André Cassiano <andre.p.cassiano@accenture.com>
2022-02-19 09:56:57 -0300

    Iniciando com spring boot

---
+++ SEF - Participants Workspace/testespring/src/test/java/springteste/testespring/TestespringApplicationTests.java
@@ -0,0 +1,13 @@
+package springteste.testespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
