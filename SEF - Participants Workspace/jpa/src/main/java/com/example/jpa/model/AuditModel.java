fd035540d211f4df9a29eb0777d14b1881af3b1b
André Cassiano <andre.p.cassiano@accenture.com>
2022-02-28 17:00:06 -0300

    Projeto JPA

---
+++ SEF - Participants Workspace/jpa/src/main/java/com/example/jpa/model/AuditModel.java
@@ -0,0 +1,65 @@
+package com.example.jpa.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+public abstract class AuditModel implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+//    @Column(name = "created_at")
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+//    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Date updatedAt;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    @PreUpdate
+    public void preUpdate() {
+//      data atualização
+        this.setUpdatedAt(new Date());
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+//      data criação e atualização
+        this.setCreatedAt(atual);
+        this.setUpdatedAt(atual);
+    }
+}
